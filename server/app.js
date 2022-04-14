169010a7a7751e73fd779625557a10c190bfc665
EssenceBrown <essence@cccareers.org>
2022-04-14 14:10:31 -0400

    added serverto my log path

--- server/app.js
+++ server/app.js
@@ -1,20 +1,47 @@
 const express = require('express');
 const fs = require('fs');
+const csv = require('csvtojson');
+//const { Agent } = require('http');
 const app = express();
 
-app.use((req, res, next) => {
-// write your logging code here
 
-});
+app.set('json spaces', 2);
 
 app.get('/', (req, res) => {
-// write your code to respond "ok" here
-
+    let Agent =req.headers['user-agent'].split(',').join('');
+    let Time = new Date().toISOString();
+    let Method = req.method;
+    let Resource = req.path;
+    let Version ='HTTP/' + req.httpVersion;
+    let Status =200;
+    // write your code to respond "ok" here
+    let logs = '\n' + Agent  + ',' + Time + ',' + Method + ',' + Resource + ',' + Version + ',' + Status;
+    console.log(logs)
+    fs.appendFile('./server/logs.csv',logs ,(err)=> {
+        if(err)throw err;
+        
+        //if no error 
+        //console.log(data)
+        //next();
+        res.status(200).send('ok')
+    })
 });
 
-app.get('/logs', (req, res) => {
+app.get('/logs', (req, res)=>{
+        csv()
+    .fromFile('./server/logs.csv')
+    .then((jsonObj)=>{
+        res.json(jsonObj)
+    })
+    
+    
+
+});
 // write your code to return a json object containing the log data here
-
-});
-
+// Use fs.readFile() method to read the file
+/* fs.readFile('./log.csv',data,'utf8',(err)=>{
+    if(err)throw err;
+    next();
+// console.log(data);*/
 module.exports = app;
+
